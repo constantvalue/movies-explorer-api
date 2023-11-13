639afc4b33a54e0e52d7f4fe582960236617c8b4
Artem Voronin <rainarlp@yandex.ru>
2023-11-13 19:15:04 +0300

    fix: fix controller bug before review

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,40 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const NotFoundError = require('../errors/NotFoundError');
-const BadRequestError = require('../errors/BadRequestError');
-const ConflictError = require('../errors/ConflictError');
-const {
-  CREATED,
-} = require('../utils/error_codes');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const NotFoundError = require("../errors/NotFoundError");
+const BadRequestError = require("../errors/BadRequestError");
+const ConflictError = require("../errors/ConflictError");
+const { CREATED } = require("../utils/error_codes");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.addUser = (req, res, next) => {
-  const {
-    name, password, email,
-  } = req.body;
+  const { name, password, email } = req.body;
 
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name, password: hash, email,
-    }))
-    .then((user) => res.status(CREATED)
-      .send({
-
-        email: user.email, name: user.name,
-
-      }))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        password: hash,
+        email,
+      })
+    )
+    .then((user) =>
+      res.status(CREATED).send({
+        email: user.email,
+        name: user.name,
+      })
+    )
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new BadRequestError('Некорректный запрос'));
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Некорректный запрос"));
         return;
-      } if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже зарегестрирован'));
+      }
+      if (err.code === 11000) {
+        next(
+          new ConflictError("Пользователь с таким email уже зарегестрирован")
+        );
         return;
       }
 
@@ -39,18 +43,17 @@ module.exports.addUser = (req, res, next) => {
 };
 
 module.exports.getUserById = (req, res, next) => {
-  User
-    .findById(req.user._id)
+  User.findById(req.user._id)
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
+        throw new NotFoundError("Нет пользователя с таким id");
       } else {
         res.send(user);
       }
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequestError('Некорректный запрос'));
+      if (err.name === "CastError") {
+        next(new BadRequestError("Некорректный запрос"));
         return;
       }
       next(err);
@@ -58,24 +61,30 @@ module.exports.getUserById = (req, res, next) => {
 };
 
 module.exports.updateProfile = (req, res, next) => {
-  User.findByIdAndUpdate(req.user._id, {
-    email: req.body.email,
-    name: req.body.name,
-  }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      email: req.body.email,
+      name: req.body.name,
+    },
+    { new: true, runValidators: true }
+  )
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Нет пользователя с таким id');
+        throw new NotFoundError("Нет пользователя с таким id");
       }
 
-      return res.send({ data: user });
+      return res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new BadRequestError('Некорректный запрос'));
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Некорректный запрос"));
         return;
       }
       if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже зарегестрирован'));
+        next(
+          new ConflictError("Пользователь с таким email уже зарегестрирован")
+        );
         return;
       }
 
@@ -89,7 +98,11 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // создадим токен (срок действия по брифу - 7 дней)
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'top_secret', { expiresIn: '7d' });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === "production" ? JWT_SECRET : "top_secret",
+        { expiresIn: "7d" }
+      );
 
       // вернём токен
       res.send({ token });
